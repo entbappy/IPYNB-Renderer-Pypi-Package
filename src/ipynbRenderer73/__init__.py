1e2ce95ad1120fa1be76cbe988cb01a3dd6c7bde
entbappy <entbappy73@gmail.com>
2022-09-21 19:01:11 +0600

    added site renderer

--- src/ipynbRenderer73/__init__.py
+++ src/ipynbRenderer73/__init__.py
@@ -1,2 +1,2 @@
 from ipynbRenderer73.youtube import render_YouTube_video, get_time_info
-# from IPYNBrenderer.site import render_site, is_valid
+from ipynbRenderer73.site import render_site, is_valid
